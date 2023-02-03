dc2860c6ce3f7eff1fa209cc20644efe268c98f4
Dave Visser | Label A <dave.visser@labela.nl>
2023-02-03 10:49:29 +0100

    Added animation start

--- app/src/main/java/com/davelabela/pokedex/MainActivity.kt
+++ app/src/main/java/com/davelabela/pokedex/MainActivity.kt
@@ -3,31 +3,41 @@ package com.davelabela.pokedex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.*
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.IntOffset
 import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.davelabela.pokedex.pokemondetail.PokemonDetailScreen
 import com.davelabela.pokedex.pokemonlist.PokemonListScreen
 import com.davelabela.pokedex.ui.theme.PokedexTheme
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.*
 
+@OptIn(ExperimentalAnimationApi::class)
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContent {
             PokedexTheme() {
-                val navController = rememberNavController()
-                NavHost(
+                val navController = rememberAnimatedNavController()
+                AnimatedNavHost(
                     navController = navController,
-                    startDestination = "pokemon_list_screen"
+                    startDestination = "pokemon_list"
                 ) {
-                    composable("pokemon_list_screen") {
+                    composable("pokemon_list") {
                         PokemonListScreen(navController = navController)
                     }
                     composable(
