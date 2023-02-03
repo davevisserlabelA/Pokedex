b91f9102a46dc1ff03ed4da3041130780986daab
Dave Visser | Label A <dave.visser@labela.nl>
2023-02-03 11:08:17 +0100

    Added new font: Poppins

--- app/src/main/java/com/davelabela/pokedex/ui/theme/Type.kt
+++ app/src/main/java/com/davelabela/pokedex/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.davelabela.pokedex.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.davelabela.pokedex.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -25,4 +27,10 @@ val Typography = Typography(
         fontSize = 12.sp
     )
     */
+)
+
+val poppinFonts = FontFamily(
+    Font(R.font.poppins_regular),
+    Font(R.font.poppins_bold, weight = FontWeight.Bold),
+    Font(R.font.poppins_medium, weight = FontWeight.Medium)
 )
