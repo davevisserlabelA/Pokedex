b921a070b6e2a65f515b2e2cb5551ec58930d981
Dave Visser | Label A <dave.visser@labela.nl>
2023-02-06 15:10:48 +0100

    Added genbuttons and basic home screen

---
+++ app/src/main/java/com/davelabela/pokedex/components/GenButtons.kt
@@ -0,0 +1,130 @@
+package com.davelabela.pokedex.components
+
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.davelabela.pokedex.R
+import com.davelabela.pokedex.ui.theme.poppinFonts
+
+@Composable
+fun RowScope.GenButton(
+    genName: String,
+) {
+    val context = LocalContext.current
+
+    Box(
+        modifier = Modifier
+            .clip(shape = RoundedCornerShape(size = 20.dp))
+            .fillMaxSize()
+            .weight(1f)
+            .background(Color.White)
+//            .shadow(1.dp, shape = RoundedCornerShape(20.dp), clip = true)
+            .clickable {
+                Toast
+                    .makeText(context, "Test: $genName", Toast.LENGTH_SHORT)
+                    .show()
+            }
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.pokeball_light),
+            contentDescription = "PokeBall",
+            alpha = 0.4f,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .size(64.dp)
+                .align(alignment = Alignment.BottomEnd)
+                .offset(x = 8.dp, y = 10.dp)
+        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(vertical = 16.dp)
+        ) {
+            Text(
+                genName,
+                textAlign = TextAlign.Center,
+                fontSize = 16.sp,
+                fontFamily = poppinFonts,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFF013A63),
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(height = 20.dp))
+            Row(horizontalArrangement = Arrangement.SpaceBetween) {
+                Surface(color = Color.Green.copy(alpha = 0.3f), modifier = Modifier.size(24.dp)) {}
+                Surface(color = Color.Red.copy(alpha = 0.3f), modifier = Modifier.size(24.dp)) {}
+                Surface(color = Color.Blue.copy(alpha = 0.3f), modifier = Modifier.size(24.dp)) {}
+            }
+            Spacer(modifier = Modifier.height(height = 40.dp))
+        }
+
+    }
+}
+
+@Composable
+fun GenButtonsList(
+    generations: List<String>
+) {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 12.dp)
+    ) {
+        itemsIndexed(generations) { index, _ ->
+            if (index % 2 == 0) {
+                if (generations.size > index + 1) {
+                    Row() {
+                        GenButton(genName = generations[index])
+                        Spacer(modifier = Modifier.padding(8.dp))
+                        GenButton(genName = generations[index + 1])
+                    }
+                } else {
+                    Row() {
+                        GenButton(genName = generations[index])
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun GenButtonsList_Preview() {
+
+    val generations = listOf<String>(
+        "Gen I",
+        "Gen II",
+        "Gen III",
+        "Gen IV",
+        "Gen V",
+        "Gen VI",
+        "Gen VII",
+        "Gen VIII",
+        "Gen IX",
+    )
+
+    GenButtonsList(generations = generations)
+}
--- app/src/main/java/com/davelabela/pokedex/homescreen/HomeScreen.kt
+++ app/src/main/java/com/davelabela/pokedex/homescreen/HomeScreen.kt
@@ -3,6 +3,7 @@ package com.davelabela.pokedex.homescreen
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,54 +16,76 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.davelabela.pokedex.R
+import com.davelabela.pokedex.components.GenButtonsList
 import com.davelabela.pokedex.components.HomeButtonList
 import com.davelabela.pokedex.ui.theme.poppinFonts
 
 @Composable
 fun HomeScreen() {
-    Box(
+
+    val generationList = listOf<String>(
+        "Gen I",
+        "Gen II",
+        "Gen III",
+        "Gen IV",
+        "Gen V",
+        "Gen VI",
+        "Gen VII",
+        "Gen VIII",
+        "Gen IX",
+    )
+
+    Box( // Background color
         modifier = Modifier
             .fillMaxSize()
             .background(Color(0xffF5F6F7))
     ) {
-        Box(
-            modifier = Modifier
-                .size(256.dp)
-                .align(Alignment.TopEnd)
-                .offset(x = 80.dp, y = -100.dp)
-        )
-        {
-            Image(
-                painter = painterResource(id = R.drawable.pokeball_light),
-                contentDescription = "PokeBall",
-                alpha = 0.2f,
-                contentScale = ContentScale.Fit,
-                modifier = Modifier.fillMaxSize()
-            )
-        }
-        Box(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Column(){
-                Text(
-                    text = "What Pokemon \nare you looking for?",
-                    fontSize = 24.sp,
-                    fontFamily = poppinFonts,
-                    fontWeight = FontWeight.Bold,
-                    color = Color(0xFF013A63),
-                    modifier = Modifier.padding(20.dp),
-                )
-                HomeButtonList()
+
+        Column {
+            Column( // Top Items Column
+                modifier = Modifier
+                    .background(Color.White, shape = RoundedCornerShape(40.dp))
+                    .padding(bottom = 16.dp)
+            ) {
+                Box() {
+                    Image( // Pokeball Image
+                        painter = painterResource(id = R.drawable.pokeball_light),
+                        contentDescription = "PokeBall",
+                        alpha = 0.2f,
+                        contentScale = ContentScale.Fit,
+                        modifier = Modifier
+                            .size(256.dp)
+                            .align(Alignment.TopEnd)
+                            .offset(x = 80.dp, y = (-100).dp) // Not proud of this
+                    )
+                    Column() {
+                        Text(
+                            text = "What Pokemon \nare you looking for?",
+                            fontSize = 24.sp,
+                            fontFamily = poppinFonts,
+                            fontWeight = FontWeight.Bold,
+                            color = Color(0xFF013A63),
+                            modifier = Modifier.padding(20.dp),
+                        )
+                        HomeButtonList()
+                    }
+                }
+            }
+            Column(
+                modifier = Modifier.padding(top = 12.dp)
+            ) { // Bottom Items Column
                 Text(
                     text = "Generations",
                     fontSize = 24.sp,
                     fontFamily = poppinFonts,
                     fontWeight = FontWeight.Bold,
                     color = Color(0xFF013A63),
-                    modifier = Modifier.padding(20.dp),
+                    modifier = Modifier.padding(start = 16.dp, bottom = 8.dp)
                 )
+                GenButtonsList(generations = generationList)
             }
         }
+
     }
 }
 
